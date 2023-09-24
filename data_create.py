9ef4d442fbc5afdb04c7d83ac6d6777405be6b70
Andrew <andrej.salnikov.03@bk.ru>
2023-09-24 15:38:13 +0300

    feat: add file data_create.py with fuctions for creatin user information

---
+++ data_create.py
@@ -0,0 +1,19 @@
+def surname_person() -> str:
+    surname = input('Фамилия: ').title()
+    return surname
+
+def name_person() -> str:
+    name = input('Имя: ').title()
+    return name
+
+def patronymic_person() -> str:
+    patronymic = input('Отчество: ').title()
+    return patronymic
+
+def num_phone_person() -> str:
+    num_phone = input('+7')
+    return num_phone
+
+def region_addres_person() -> str:
+    addres = input('Город: ')
+    return addres
