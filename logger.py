389e44d6770a73681181d04346d88d15c69511fe
Andrew <andrej.salnikov.03@bk.ru>
2023-09-24 18:31:06 +0300

    feat: add new functions change_data() & delete_data() in logger.py

--- logger.py
+++ logger.py
@@ -32,19 +32,68 @@ def print_data():
     with open('book.txt', 'r', encoding='utf_8') as file:
         print(file.read())
 
-# def change_data():
-#     print('Вы выбрали изменить данные контакта.')
-#     print('Выберите вариант поиска контакта, чью информацию вы хотите изменить:\n'
-#           '1 - Фамилия\n'
-#           '2 - Имя\n'
-#           '3 - Отчество\n'
-#           '4 - Номер телефона\n'
-#           '5 - Адрес\n')
+def change_data():
+    print('Выберите вариант поиска контакта, чью информацию вы хотите изменить:\n'
+          '1 - Фамилия\n'
+          '2 - Имя\n'
+          '3 - Отчество\n'
+          '4 - Номер телефона\n'
+          '5 - Адрес\n')
+    index = int(input('Выберите действие: ')) - 1
+    search = input('Введите данные поиска: ').title()
+    replacement = input('Введите данные для замены: ').title()
+    with open('book.txt', 'r', encoding='utf_8') as file:
+        data = file.read().strip().split('\n\n')
     
-# def delete_data():
-#     print('Выберите вариант поиска контакта, которого хотите удалить:\n'
-#           '1 - Фамилия\n'
-#           '2 - Имя\n'
-#           '3 - Отчество\n'
-#           '4 - Номер телефона\n'
-#           '5 - Адрес\n')
+    new_data = []
+    my_check = False
+
+    for item in data:
+        contact_info = item.replace('\n', ' ').split()
+        if len(contact_info) >= index + 1:
+            if contact_info[index] == search:
+                my_check = True
+                contact_info[index] = replacement
+                new_data.append(
+                    f'{contact_info[0]} {contact_info[1]} {contact_info[2]}\n+7{contact_info[3]}\nГород: {contact_info[4]}\n')
+            else:
+                new_data.append(item)
+        else:
+            new_data.append(item)
+    
+    if not my_check:
+        print('Совпадений не найдено. Введите корректные данные')
+
+    with open('book.txt', 'w', encoding='utf_8') as file:
+        file.write('\n'.join(new_data))
+
+def delete_data():
+    print('Выберите вариант поиска контакта, которого хотите удалить:\n'
+          '1 - Фамилия\n'
+          '2 - Имя\n'
+          '3 - Отчество\n'
+          '4 - Номер телефона\n'
+          '5 - Адрес\n'
+          '6 - Удалить контакт целиком\n')
+    index = int(input('Выберите действие: ')) - 1
+    search_to_delete = input('Введите данные удаления: ').title()
+    with open('book.txt', 'r', encoding='utf_8') as file:
+        data = file.read().strip().split('\n\n')
+    
+    new_data = []
+
+    for item in data:
+        if search_to_delete in item:
+
+            if index < 5:
+                contact_info = item.replace('\n', ' ').split()
+                if len(contact_info) >= index + 1 and search_to_delete == contact_info[index]:
+                    contact_info[index] = 'deleted'
+                    new_data.append(f'{contact_info[0]} {contact_info[1]} {contact_info[2]}\n+7{contact_info[3]}\nГород: {contact_info[4]}\n')
+                else:
+                    new_data.append(item)
+        else:
+            new_data.append(item) 
+        
+    with open('book.txt', 'w', encoding='utf_8') as file:
+        file.write('\n'.join(new_data))
